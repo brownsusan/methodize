f22dc9f1aa2af93339c701a1518546a01efe2416
Susan Brown <brownsusan@fullsail.edu>
2014-04-07 17:18:48 -0400

    ...

--- backend/controller/test.js
+++ backend/controller/test.js
@@ -1,5 +1,10 @@
 module.exports.route = function(app) {
 
+	app.get('/test/session_delete', function(req, res) {
+		req.session.destroy();
+		res.end();
+	});
+
 	app.get('/test/session', function(req, res) {
 		console.log(req.session);
 		res.end();
