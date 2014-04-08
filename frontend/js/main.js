cdc54108e6acf2efcd6e4cdb04f946015ba2b11a
Susan Brown <brownsusan@fullsail.edu>
2014-04-08 00:26:04 -0400

    getting form fully functional for addiing tasks. safety commit

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	$('#tab-container').easytabs();
+	$('#task-submit-due-date').datetimepicker();
+	$('#event-submit-start-date').datetimepicker();
+	$('#event-submit-end-date').datetimepicker();
 });
 
 $('.my-nav-slide').click(function() {
