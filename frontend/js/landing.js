58cae3d133031c5939068d1f3b7653c0e535b8f4
Susan Brown <brownsusan@fullsail.edu>
2014-04-07 23:55:37 -0400

    compartmentalizing front end js

---
+++ frontend/js/landing.js
@@ -0,0 +1,74 @@
+$('#login-submit').click(function() {
+
+	var email = $('#login-email').val();
+	var pass = $('#login-pass').val();
+
+	$.ajax({
+		url : "/action/signin_user",
+		type : "post",
+		dataType : "json",
+		data : {
+			'email' : email,
+			'password' : pass
+		},
+		success : function(response) {
+			if (!response.error) {
+				window.location.href = "/calendar/week";
+			}
+		}
+	});
+
+});
+
+$('#sign-up-submit').click(function() {
+	var email = $('#sign-up-email').val();
+	var phone = $('#sign-up-phone').val();
+	var pass = $('#sign-up-pass').val();
+	var confirmPass = $('#sign-up-confirmPass').val();
+
+	var regex_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	var regex_pass = /^[a-zA-Z]\w{3,14}$/;
+
+	valid = true;
+
+	if (!regex_email.test(email)) {
+		valid = false;
+	}
+
+	if (!regex_pass.test(pass)) {
+		valid = false;
+	}
+
+	if (confirmPass != pass) {
+		valid = false
+	}
+
+	if (email.length === 0 || phone.length === 0 || pass.length === 0 || confirmPass === 0) {
+		valid = false;
+	}
+
+	if (valid == false) {
+		console.log("invalid");
+		var errorHtml = '<span>Please fill out the form completely and correctly. Be sure the check the hints in each field!<span></br>';
+		$('#landing-error').html(errorHtml);
+		return;
+	}
+
+	$.ajax({
+		url : "/action/create_user",
+		type : "post",
+		dataType : "json",
+		data : {
+			'email' : email,
+			'phone' : phone,
+			'password' : pass
+		},
+		success : function(response) {
+			console.log(response.error);
+			if (!response.error) {
+				window.location.href = "/calendar/week";
+			}
+		}
+	});
+
+});
