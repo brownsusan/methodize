bba63d3b1ca66727e56f99a7906a68504f2f12d5
Susan Brown <brownsusan@fullsail.edu>
2014-05-30 15:24:45 -0400

    commit

--- tasktest.js
+++ tasktest.js
@@ -3,34 +3,63 @@ var twilio = require("twilio");
 //TWILIO CONCEPT
 // TODO
 // Set the interval to check the tasks and events
-// window.setInterval(function() {checkEventsAndTasks()},3000);
-
-// TODO
-// Check the events and tasks
-// var checkEventsAndTasks = function() {
-	// TODO
-	// Read all tasks and events in the database for EVERY USER
+var processing = false;
+//Every thirty seconds do this
+	//Emit Socket Read Event
+	//set processing to true
+	
+// Outside of set interval
+//On Socket Complete Event 
+	//set processing to false,
 	// Loop over the returned events and tasks
 		// Check to see if any reminders exists
 		// If so, loop over the reminders
 			// In the loop - Check and store the reminder start date
 			// In the loop - Check and store the via array
 			// If the reminder start date is within 30 seconds of the current time and the frequency is once
-				// If via.phone is true
-				// Send out a call
-				// If via.sms is true
-				// Send out an sms
+					// If via.phone is true
+					// Send out a call
+					// If via.sms is true
+					// Send out an sms
 			// If the reminder start date is within 30 seconds and the frquency is not once
-				// Set another interval dependant on the frequency field
-				// if via.phone is true
-				// Send out a call
-				// if via.sms is true
-				// Send out an sms
-				// Set timeout for reminder end date/time	
+					// Set another interval dependant on the frequency field
+					// if via.phone is true
+					// Send out a call
+					// if via.sms is true
+					// Send out an sms
+					// Set timeout for reminder end date/time
+// console.log('Checking the events and tasks');)
+
+window.setInterval(function() {
+	if (!processing) {
+		checkEventsAndTasks();
+	}
+}, 3000);
+
+// TODO
+// Check the events and tasks
+// var checkEventsAndTasks = function() {
+// TODO
+// Read all tasks and events in the database for EVERY USER
+// Loop over the returned events and tasks
+// Check to see if any reminders exists
+// If so, loop over the reminders
+// In the loop - Check and store the reminder start date
+// In the loop - Check and store the via array
+// If the reminder start date is within 30 seconds of the current time and the frequency is once
+// If via.phone is true
+// Send out a call
+// If via.sms is true
+// Send out an sms
+// If the reminder start date is within 30 seconds and the frquency is not once
+// Set another interval dependant on the frequency field
+// if via.phone is true
+// Send out a call
+// if via.sms is true
+// Send out an sms
+// Set timeout for reminder end date/time
 // console.log('Checking the events and tasks');
 // }
-
-
 
 // This is the testing account info
 
